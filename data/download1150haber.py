57334f2aab1640b07088aee95494c2e0e8f0b9b4
Aydın Gerek <ayg207@gmail.com>
2021-03-07 15:01:32 +0300

    1150haber downloader can now write csv file and jsonnet for fine tuning it in BERT works

--- data/download1150haber.py
+++ data/download1150haber.py
@@ -3,14 +3,33 @@
 import argparse
 from glob import glob
 import os
+import pandas as pd
 import re
+from sklearn.model_selection import train_test_split
+
+#TODO write actual download code.
 
 def create_csv(path):
     """Given downloaded model's root path this will create a csv file from it and save it there"""
     raw_texts = os.path.join(path, 'raw_texts')
     fnames = {re.search('[^/]+$', dir)[0]: sorted(glob(dir+'/*'))
               for dir in sorted(glob(os.path.join(raw_texts,'*')))}
+
+    data = []
+    for label, files in fnames.items():
+        for file in files:
+            with open(file, encoding='iso-8859-9') as inpf:
+                text = inpf.read().replace("\'", "").replace('\x92', "").replace('\x91', "").replace("\n", " ")
+                data.append({'label':label, 'text': text, 'filename':file})
     
+    data = pd.DataFrame(data)
+    data.to_csv(os.path.join(path, '1150haber.csv'))
+    traindev, test = train_test_split(data,test_size = 200, random_state=1848, shuffle=True, stratify=data.label)
+    traindev.to_csv(os.path.join(path, '1150haber_traindev.csv'))
+    test.to_csv(os.path.join(path, '1150haber_test.csv'))
+    train,    dev  = train_test_split(traindev, test_size = 200, random_state=1848, shuffle=True, stratify=traindev.label)
+    train.to_csv(os.path.join(path, '1150haber_train.csv'))
+    dev.to_csv(os.path.join(path, '1150haber_dev.csv'))
 
 
 if __name__ == '__main__':
